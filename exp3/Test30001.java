9d16d4e461ad6f2e28694d70167346c0df60d40b
zjg-stu-lizheng <3190105300@zju.edu.cn>
2022-03-15 16:57:35 +0800

    modify readme.md

--- exp3/Test30001.java
+++ exp3/Test30001.java
@@ -1,18 +1,18 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class Test30001 {
-// public static void main(String[] args) {
-// int ri, repeat;
-// int mark;
-// Scanner in = new Scanner(System.in);
-// repeat = in.nextInt();
-// for (ri = 1; ri <= repeat; ri++) {
-// mark = in.nextInt();
-// if (mark < 60) {
-// System.out.println("Fail");
-// } else {
-// System.out.println("Pass");
-// }
-// }
-// }
-// }
+public class Test30001 {
+    public static void main(String[] args) {
+        int ri, repeat;
+        int mark;
+        Scanner in = new Scanner(System.in);
+        repeat = in.nextInt();
+        for (ri = 1; ri <= repeat; ri++) {
+            mark = in.nextInt();
+            if (mark < 60) {
+                System.out.println("Fail");
+            } else {
+                System.out.println("Pass");
+            }
+        }
+    }
+}
